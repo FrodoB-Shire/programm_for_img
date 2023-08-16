80976233d8a31e0276ca9964cbafc028917ef862
Константин Сенцов <konstantin.s25212@icloud.com>
2023-08-16 16:00:30 +0300

    Добавил ткинтер с рабочим Entry

--- bulls_and_cows.py
+++ bulls_and_cows.py
@@ -1,4 +1,5 @@
 import random
+import tkinter as tk
 
 def new_number(hard):
     numb = set()
@@ -7,11 +8,26 @@ def new_number(hard):
         numb.add(str(n))
     return ''.join(list(numb))
 
-start_game = input('Выбери уровень сложности: Легко, нажми - 3, Средне - 4, Сложно - 5')
-hidden_number = new_number(int(start_game))
 
-user = input(f'Введи {len(hidden_number)}-значное число: ')
-while len(user) != len(hidden_number) or user.isalpha():
-    user = input('Введи корректное число: ')
+def get_num():
+    return user.get()
+
+win = tk.Tk()
+win.geometry('500x500')
+
+
+label = tk.Label(win, text='Введи число').pack()
+user = tk.Entry(win)
+user.pack()
+btn1 = tk.Button(win, text='get', command=get_num).pack()
+
+
+#start_game = input('Выбери уровень сложности: Легко, нажми - 3, Средне - 4, Сложно - 5')
+#hidden_number = new_number(int(start_game))
+#user = input(f'Введи {len(hidden_number)}-значное число: ')
+#while len(user) != len(hidden_number) or user.isalpha():
+#    user = input('Введи корректное число: ')
+
+
 
 
